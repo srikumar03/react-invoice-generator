e9184e6a0fdb417212fcc2851ced11ab0b784999
SRIDHAR KUMAR <96507579+srikumar03@users.noreply.github.com>
2023-12-09 21:36:02 +0530

    Add files via upload

---
+++ precache-manifest.4d40f0433c4cd2b6d5e6570d69b5e746.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924e18c747c3c386a8e873592d204dff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56708912e8740f93c9b7",
+    "url": "/static/css/3.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "24d6635ce60f726112b2",
+    "url": "/static/css/main.5313372f.chunk.css"
+  },
+  {
+    "revision": "0c004e7a60307f97d6e6",
+    "url": "/static/js/2.69b469c9.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/2.69b469c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56708912e8740f93c9b7",
+    "url": "/static/js/3.a3dacca7.chunk.js"
+  },
+  {
+    "revision": "fcaea4cefd65553c3d85400ff3ed2886",
+    "url": "/static/js/3.a3dacca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a023b361e155b7fbfc52",
+    "url": "/static/js/4.07e532be.chunk.js"
+  },
+  {
+    "revision": "a76e873f1b2326391d665076330da62d",
+    "url": "/static/js/4.07e532be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f357b09f91543c85fc8c",
+    "url": "/static/js/5.6c0eb888.chunk.js"
+  },
+  {
+    "revision": "24d6635ce60f726112b2",
+    "url": "/static/js/main.f7e99734.chunk.js"
+  },
+  {
+    "revision": "8d94bfeaa3fccdc10d9e",
+    "url": "/static/js/runtime-main.10b73ebd.js"
+  }
+]);
